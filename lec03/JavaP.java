0ac142481547a22262118c6fa135257abe2fa80b
Yang Ge <eroicaleo@gmail.com>
2015-05-17 00:16:16 -0700

    Added JavaP.java

---
+++ lec03/JavaP.java
@@ -0,0 +1,9 @@
+public class JavaP {
+    public static void main(String[] s) {
+        int ia = 2;
+        int ib = ia++ + ++ia;
+        boolean ba = true;
+        boolean bb = false;
+        System.out.println(ba+","+bb);
+    }
+}
